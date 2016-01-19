39cf31f869bcd115781ff9d1cc851b842b858ca8
HuttonR <hutton.rounds@thebronson.com>
2016-01-18 18:39:26 -0800

    Added Tether.

--- refreshbootstrap.sh
+++ refreshbootstrap.sh
@@ -1,9 +1,11 @@
 bootstrapVersion="v4.0.0-alpha.2"
+tetherVersion="v1.1.1"
 
-echo "Initializing bootstrap submodule... ($bootstrapVersion)"
 git submodule update --init
 
-submodulepath=.git/modules/$(grep -oP "path\s*\=\s*\K(.+)" .gitmodules)
+
+echo "Initializing bootstrap submodule... ($bootstrapVersion)"
+submodulepath=.git/modules/assets/bootstrap
 
 $(cd $submodulepath; git config core.sparsecheckout true)
 
@@ -13,6 +15,21 @@ echo 'scss/*.scss' >> $submodulepath/info/sparse-checkout
 echo 'scss/mixins/*.scss' >> $submodulepath/info/sparse-checkout
 
 $(cd $submodulepath; git pull; git checkout $bootstrapVersion)
+
+
+
+echo "Initializing tether submodule... ($tetherVersion)"
+submodulepath=.git/modules/assets/tether
+
+$(cd $submodulepath; git config core.sparsecheckout true)
+
+echo '' > $submodulepath/info/sparse-checkout
+echo 'dist/js/tether.js' >> $submodulepath/info/sparse-checkout
+
+$(cd $submodulepath; git pull; git checkout $tetherVersion)
+
+
+
 
 echo "Generating asset list for package.js..."
 
@@ -29,5 +46,11 @@ echo "Generating js filenames for serve.js..."
 bootstrapJsFilenames="$(find assets/bootstrap -not -path '*/\.*' -name '*.js' -type f -printf "'%f'\n" | grep -v '^$' | paste -s -d ",")"
 sed '/jsAssetNames.*=.*/d' -i serve.js
 echo "var jsAssetNames = [$bootstrapJsFilenames];" >> serve.js
+
+
+tetherJsFiles="$(find assets/tether -not -path '*/\.*' -name '*.js' -type f -exec echo "'{}'" \; | grep -v '^$' | paste -s -d ',')"
+sed '/tetherJsAssets.*=.*/d' -i package.js
+echo "var tetherJsAssets = [$tetherJsFiles];" >> package.js
+
 
 echo "Done"
